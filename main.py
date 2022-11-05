208590cfae5d75f1a1b96b2c9c9807af98a6eb87
Adrien Tremblay <adrien@adrientremblay.com>
2022-11-05 17:31:04 -0400

    some file reading

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def parse_matrix(matrix_line):
+    ret = [[0]*6 for i in range(6)] ;
+    cur = 0;
 
-# Press the green button in the gutter to run the script.
+    for i in range(6):
+        for j in range(6):
+            ret[i][j] = matrix_line[cur];
+            cur += 1
+
+    return ret;
+
+def parse_file(file_name):
+    matrices = [];
+
+    file1 = open(file_name, 'r')
+    lines = file1.readlines()
+
+    for line in lines:
+        if line == '\n' or line[0] == '#':
+            continue
+
+        matrices.append(parse_matrix(line))
+
+    return matrices
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print(parse_file('sample_input_1.txt'))
