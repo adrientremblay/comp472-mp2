76cad97dfcac891cc8ee7d9fbafecdc99139a65f
Amine Halimi <35849317+kilner99@users.noreply.github.com>
2022-11-26 13:28:54 -0500

    Feature: heuristic 4 implemented. Heuristic functions accept a node object rather than the board itself. Accepts a multiplier variable (int) for heuristic 3 for all heuristic functions (for homogeneous sake)

--- algo_a_astar.py
+++ algo_a_astar.py
@@ -2,9 +2,9 @@ from node import Node
 from fifo_priority_queue import FifoPriorityQueue
 import heuristics
 
-def algo_a_astar(start_node, estimator=heuristics.check_heuristic1):
+def algo_a_astar(start_node, estimator=heuristics.check_heuristic1, mult=1):
     open_list = FifoPriorityQueue()
-    open_list.put(estimator(start_node.board) + start_node.cost, start_node)
+    open_list.put(estimator(start_node,multiplier=mult) + start_node.cost, start_node)
     closed_list = []
     search_list = []
 
@@ -12,16 +12,16 @@ def algo_a_astar(start_node, estimator=heuristics.check_heuristic1):
         next_pair = open_list.get()
 
         if next_pair[1].is_goal_state():
-            search_list.append((next_pair[0], next_pair[1].cost, estimator(next_pair[1].board), next_pair[1]))
+            search_list.append((next_pair[0], next_pair[1].cost, estimator(next_pair[1]), next_pair[1]))
             return next_pair, search_list
 
         for child_node in next_pair[1].generate_moves():
             if child_node.board in closed_list:
                 continue
 
-            open_list.put_with_replacement(estimator(child_node.board) + child_node.cost, child_node)
+            open_list.put_with_replacement(estimator(child_node,multiplier=mult) + child_node.cost, child_node)
 
         closed_list.append(next_pair[1].board)
-        search_list.append((next_pair[0], next_pair[1].cost, estimator(next_pair[1].board), next_pair[1]))
+        search_list.append((next_pair[0], next_pair[1].cost, estimator(next_pair[1],multiplier=mult), next_pair[1]))
 
     return None, search_list
