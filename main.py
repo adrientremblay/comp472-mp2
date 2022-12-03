286946f9d52d11d12a321abe03b81a016650412e
RosscoPico <34090996+RosscoPico@users.noreply.github.com>
2022-12-02 19:53:50 -0500

    Updates Solution Files
    Added No solution

--- main.py
+++ main.py
@@ -95,16 +95,47 @@ def create_solution_file(search_list, search_algo_name, test_number, node_to_tes
         file_name += "-" + heuristic_name
     file_name += "-sol-" + str(test_number) + ".txt"
     soultion_file = open(file_name, "w")
+    
+    no_solution = False
+    try:
+        solution = solution[1]
+    except TypeError:
+        solution = None
+        no_solution = True
 
+    solution_path = []
+    cur = solution
+    while cur is not None and cur.parent is not None:
+        solution_path.insert(0 , cur)
+        cur = cur.parent
+    
     sol_txt = f"Initial Board Configuration: {lines} \n"
     sol_txt += f"{node_to_test.board_to_string()}\n" 
     sol_txt += f"Car fuel avalaible: {[(k,v.fuel) for (k,v) in sorted(node_to_test.cars.items())]} \n" # fuel
     sol_txt += f"Runtime: {round(eTime,1) } seconds \n" # uses time in the method
     sol_txt += f"Search path length: {len(search_list)} states\n" # search path
-    sol_txt += f"Solution path length: moves \n" #missing
-    sol_txt += f"Solution Path length: \n" #missing
-    sol_txt += f"Solution Path: \n\n\n" #missing
-    sol_txt += f"{solution.board_to_string()}\n" #solution board
+    
+    if no_solution:
+        sol_txt += f"\n No solution found.\n"
+    else:
+
+        sol_txt += f"Solution path length: {len(solution_path)}\n" #missing
+        sol_txt += f"Solution Path: " #missing
+        for n in solution_path :
+            sol_txt += n.move_name + "; " 
+    
+        sol_txt += f"\n\n"
+    
+        for n in solution_path :
+            sol_txt += n.move_name + "\t" + str(n.move_fuel)
+            for i in range(0,6):
+                for j in range(0, 6):
+                    sol_txt += n.board[i][j]
+            sol_txt += f"\n"   
+        
+
+        sol_txt += f"\n"
+        sol_txt += f"{solution.board_to_string()}\n" #solution board
 
     soultion_file.write(sol_txt)
     soultion_file.close()
@@ -136,7 +167,7 @@ if __name__ == '__main__':
             if GENERATE_SEARCH_FILE:
                 create_search_file(search_list=search_list, search_algo_name="ucs", test_number=i+1)
             if GENERATE_SOLUTION_FILE:
-                create_solution_file(search_list=search_list, search_algo_name="ucs", test_number=i+1,node_to_test = node_to_test,lines = lines[i],solution = solution[1],eTime= end-start)
+                create_solution_file(search_list=search_list, search_algo_name="ucs", test_number=i+1,node_to_test = node_to_test,lines = lines[i],solution = solution,eTime= end-start)
             if GENERATE_CSV:
                 if solution is None:
                     csv_data.append([i+1, "UCS", "NA", "NA", "NA", node_to_test.to_line()])
@@ -162,7 +193,7 @@ if __name__ == '__main__':
                 if GENERATE_SEARCH_FILE:
                     create_search_file(search_list=search_list,search_algo_name="gbsf",test_number=i+1)
                 if GENERATE_SOLUTION_FILE:
-                    create_solution_file(search_list=search_list, search_algo_name="ucs", test_number=i+1,node_to_test = node_to_test,lines = lines[i],solution = solution[1],eTime= end-start)
+                    create_solution_file(search_list=search_list, search_algo_name="ucs", test_number=i+1,node_to_test = node_to_test,lines = lines[i],solution = solution,eTime= end-start)
                 if GENERATE_CSV:
                     if solution is None:
                         csv_data.append([i+1, "UCS", "NA", "NA", "NA", node_to_test.to_line()])
@@ -192,7 +223,7 @@ if __name__ == '__main__':
                 if GENERATE_SEARCH_FILE:
                     create_search_file(search_list=search_list,search_algo_name="a",test_number=i+1)
                 if GENERATE_SOLUTION_FILE:
-                    create_solution_file(search_list=search_list, search_algo_name="ucs", test_number=i+1,node_to_test = node_to_test,lines = lines[i],solution = solution[1],eTime= end-start)
+                    create_solution_file(search_list=search_list, search_algo_name="ucs", test_number=i+1,node_to_test = node_to_test,lines = lines[i],solution = solution,eTime= end-start)
                 if GENERATE_CSV:
                     if solution is None:
                         csv_data.append([i+1, "UCS", "NA", "NA", "NA", node_to_test.to_line()])
